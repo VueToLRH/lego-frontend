4ea2d7166be28ce608df808ab1ff70ce989a5481
lrh21g <lingronghai@foxmail.com>
2024-03-17 20:24:15 +0800

    feat: 全局注册 AntDesignVue 以及组件声明

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 
 import router from './router'
@@ -6,6 +7,7 @@ import store from './store'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(Antd)
 app.use(router)
 app.use(store)
 app.mount('#app')
